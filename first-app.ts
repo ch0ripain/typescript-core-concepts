4c87a4277689fcca2f95adea61d80484fc6f6355
Leandro Rufino <leandrix1234@gmail.com>
2025-05-15 11:57:48 -0300

    object types

--- first-app.ts
+++ first-app.ts
@@ -1,9 +1,28 @@
 let userName = "Leo"; // Infer type string | let userName: string; && number & boolean
 
-//Union types
+// Union types
 let userId: string | number; // Concatenate at least two types
 userId = 123;
 userId = "321";
 // userId = true
 // userId = []
 // userId = {}
+
+// Object types
+
+// let user: object;
+let user: {
+  name: string;
+  age: number;
+  isAdmin: boolean;
+  id: number | string;
+};
+
+// user = "Leo" => Error
+// user = {} => Error - Empty object
+user = {
+  name: "Leo",
+  age: 21,
+  isAdmin: true,
+  id: "123",
+};
