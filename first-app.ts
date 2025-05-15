735a755af2eae44b72c63917860f6a2c278b9aa9
Leandro Rufino <leandrix1234@gmail.com>
2025-05-15 11:47:16 -0300

    union types

--- first-app.ts
+++ first-app.ts
@@ -1,1 +1,9 @@
 let userName = "Leo"; // Infer type string | let userName: string; && number & boolean
+
+//Union types
+let userId: string | number; // Concatenate at least two types
+userId = 123;
+userId = "321";
+// userId = true
+// userId = []
+// userId = {}
